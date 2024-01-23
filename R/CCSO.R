51d79abcba24e6cd8fec0334312c30f475a894eb
Daniel Eck <deck1388@gmail.com>
2024-01-23 17:22:25 -0600

    add CCSO data

---
+++ R/CCSO.R
@@ -0,0 +1,12 @@
+#' Champaign County Sheriff's Office Booking Data
+#'
+#' Arrest data obtained from the Champaign County Sheriff's Office 
+#' located in Champaign, IL Data collected from Jan 1, 2011 through 
+#' December 31, 2016. 
+#'
+#' @usage data(CCSO)
+#'
+#' @format A tibble with 67764 observations of 31 variables:
+#'
+#' @source https://data.illinois.gov/dataset/jail-booking-data
+"CCSO"
