7119254937287b325f8531178a20e153d14ba584
Daniel Eck <deck1388@gmail.com>
2024-01-23 21:27:29 -0600

    updated documentation on CCSO data set

--- R/CCSO.R
+++ R/CCSO.R
@@ -7,6 +7,25 @@
 #' @usage data(CCSO)
 #'
 #' @format A tibble with 67764 observations of 31 variables:
+#' \itemize{
+#'   \item[bookingDate] A date variable (formatted as yyyy-mm-dd) 
+#'   containing the date of arrest.
+#'   \item[jacketNumber] A numeric vector designating a unique 
+#'   identifier for the person being arrested.
+#'   \item[bookingNumber] A numeric variable indicating the booking 
+#'   number. 
+#'   \item[bookingTime] A time variable (formatted as hh:mm:ss) 
+#'   containing the time of arrest.
+#'   \item[custodyClass] A character variable indicating the custody 
+#'   classification of the arrested individual. This is a 
+#'   designation that indicates the extent of security measures that 
+#'   must be in place for an inmate of a particular classification. 
+#'   Missing values are associated with short stays.
+#'   \item[employmentStatus] A character vector indicating the 
+#'   employment status of the individual. 
+#'   \item[incarcerationReason] A character vector indicated the 
+#'   reason the arrest.
+#' }
 #'
 #' @source https://data.illinois.gov/dataset/jail-booking-data
 "CCSO"
